d8f8340698c4ffbfe890e12a3c93adcf8aaf6dbf
khajamisabh057 <khajamisbah057@gmaol.com>
2024-10-22 13:08:20 +0530

    22-10-2024

--- SD-Hub-Portal/src/app/registration/registration.component.ts
+++ SD-Hub-Portal/src/app/registration/registration.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.css'
+  styleUrl: './ '
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
+  isLinear = false;
+  firstFormGroup!:FormGroup;
+  secondFormGroup!:FormGroup;
 
+  constructor(
+    private _formBuilder: FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required],
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required],
+    });
+  }
 }
