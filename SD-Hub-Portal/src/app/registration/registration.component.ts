7320ca97cd1607f1b12b50d401e0741b386f820c
khajamisabh057 <khajamisbah057@gmaol.com>
2024-10-26 13:03:21 +0530

    26-10-2024

--- SD-Hub-Portal/src/app/registration/registration.component.ts
+++ SD-Hub-Portal/src/app/registration/registration.component.ts
@@ -1,26 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, Validators, FormGroup} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrl: './ '
+  templateUrl:'./registration.component.html',
+  styleUrl: './registration.component.css'
 })
 export class RegistrationComponent implements OnInit {
-  isLinear = false;
-  firstFormGroup!:FormGroup;
-  secondFormGroup!:FormGroup;
+  firstFormGroup!: FormGroup;
+  secondFormGroup!: FormGroup;
+  thirdformGroup!: FormGroup;
+  // FourthformGroup!: FormGroup;
 
-  constructor(
-    private _formBuilder: FormBuilder
-  ){}
+    constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
+    ngOnInit(): void{
+      this.firstFormGroup = this.fb.group({
+        studentID: ['', [Validators.required]],
+        appDate: ['', [Validators.required]],
+        firstName: ['', [Validators.required]],
+        middleName: ['', [Validators.required]],
+        lastName: ['', [Validators.required]],
+        f_firstName: ['', [Validators.required]],
+        f_middleName: ['', [Validators.required]],
+        f_lastName: ['', [Validators.required]],
+        dob: ['', [Validators.required]],
+        email: ['', [Validators.required]],
+        address: ['', [Validators.required]],
+        income: ['', [Validators.required]],
+        p_contact: ['', [Validators.required]],
+        phoneNumber: ['', [Validators.required]],
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required],
+    
+
+      this.secondFormGroup = this.fb.group({
+        Degree: ['', [Validators.required]],
+        Collegename: ['', [Validators.required]],
+        Year: ['', [Validators.required]],
+        Percentage: ['', [Validators.required]]
+      }); 
+  
+
+    this.thirdformGroup = this.fb.group({
+    studentsign: ['', [Validators.required]],
+    Parentsign: ['', [Validators.required]]
     });
-  }
+
+    // this.FourthformGroup = this.fb.group({
+    //   button:['',[Validators.required]]
+
+    // });
+    
+
+  
+    }
 }
